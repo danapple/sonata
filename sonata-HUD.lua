39d01cd51874d2149fd6c43fd70ac044cd6cd0cd
Daniel I. Applebaum <danapple@wotke.danapple.com>
2020-06-06 17:08:38 +0200

    Separate primary HUD from autopilot HUD.  Rename files for clarity.

--- sonata-HUD.lua
+++ sonata-HUD.lua
@@ -19,16 +19,7 @@ require "bit"
   local speedbrakes_y = 60
 
 dataref( "view_type", "sim/graphics/view/view_type" )
-dataref("ALT_ARMED", "sim/cockpit2/autopilot/altitude_hold_armed")
 dataref("ap_state", "sim/cockpit/autopilot/autopilot_state", "readonly")
-dataref("xp_autothrottle_enabled", "sim/cockpit2/autopilot/autothrottle_enabled", "writable")
-dataref("QNH_Pilot", "sim/cockpit2/gauges/actuators/barometer_setting_in_hg_pilot", "writable")
-dataref("ap_mode", "sim/cockpit/autopilot/autopilot_state", "readonly")
-
-local headingWindow = XPLMFindDataRef("sim/cockpit/autopilot/heading_mag")
-local speedWindow = XPLMFindDataRef("sim/cockpit/autopilot/airspeed")
-local altWindow = XPLMFindDataRef("sim/cockpit/autopilot/altitude")
-local vsWindow = XPLMFindDataRef("sim/cockpit/autopilot/vertical_velocity")
 
 local throttlePositionDR = XPLMFindDataRef("sim/flightmodel2/engines/throttle_used_ratio")
 local n1percentDR = XPLMFindDataRef("sim/cockpit2/engine/indicators/N1_percent")
@@ -79,94 +70,24 @@ local flaps_deployed_DR = XPLMFindDataRef("sim/flightmodel2/controls/flap1_deplo
 local speedbrakes_handle_DR = XPLMFindDataRef("sim/cockpit2/controls/speedbrake_ratio")
 local speedbrakes_deployed_DR = XPLMFindDataRef("sim/flightmodel2/controls/speedbrake_ratio")
 
-isMach = XPLMFindDataRef("sim/cockpit/autopilot/airspeed_is_mach")
-
-function render(spd, isFraction)
-  if isFraction == 1 then
-    return string.format("%1.3f", spd)
-  else
-    return string.format("%03i", spd)
-  end
-end
-
 function draw_sonata_hud()
-  local isMachNow = XPLMGetDatai(isMach)
 
 -- are we in HUD view?
   if view_type ~= 1023 then
     return
   end
 
---  if XPLMGetDatai(ap_mode) < 2 then
---    return
---  end
-
   ssWidth, ssHeight = XPLMGetScreenSize()
 
   XPLMSetGraphicsState(0,0,0,1,1,0,0)
 
   autothrottleEngaged = bit.band(ap_state, 1)
-  headingHoldEngaged = bit.band(ap_state, 2)
-  vviEngaged = bit.band(ap_state, 16)
-  flChgEngaged = bit.band(ap_state, 64)
-  altHoldEngaged = bit.band(ap_state, 16384)
-
-  -- draw some text
-  local x_pos = ssWidth * 2.85 / 10
-  local y_pos = ssHeight * 6.35 / 10
-  if isMachNow == 1 then
-     y_pos = ssHeight * 3.3 / 10
-  end   
-  if autothrottleEngaged > 0 then
-    graphics.set_color(1,0,1,1)
-  else
-    graphics.set_color(0,1,0,1)
-  end  
-  local speed = XPLMGetDataf(speedWindow)
-  draw_string_Helvetica_12(x_pos, y_pos, render(speed, isMachNow))
-
-  if altHoldEngaged > 0 then
-    graphics.set_color(0,0,1,1)
-  elseif flChgEngaged > 0 then
-    graphics.set_color(1,0,1,1)
-  elseif vviEngaged > 0 then
-    graphics.set_color(1,0,0,1)
-  else
-    graphics.set_color(0,1,0,1)
-  end
-  alt = XPLMGetDataf(altWindow)
-  draw_string_Helvetica_12(ssWidth * 6.85 / 10, ssHeight * 6.35 / 10, string.format("%5i", alt))
-
-  vvi = XPLMGetDataf(vsWindow)
-  if vviEngaged > 0 or vvi < -.5 or vvi > .5 then
-    if vviEngaged > 0 then
-      graphics.set_color(0,0,1,1)
-    elseif vvi < -.5 or vvi > .5 then
-      graphics.set_color(1,0,1,1)
-    else
-      graphics.set_color(0,1,0,1)
-    end
-    draw_string_Helvetica_12(ssWidth * 7.6 / 10, ssHeight * 4.9 / 10, string.format("%5i", vvi))
-  end
-  
-  hdg = XPLMGetDataf(headingWindow)
-  if headingHoldEngaged > 0 then
-    graphics.set_color(1,0,1,1)
-  else  
-    graphics.set_color(0,1,0,1)
-  end
-  draw_string_Helvetica_12(ssWidth * 5 / 10, ssHeight * 1 / 10, string.format("%03d", hdg))
 
   draw_throttle()
-
   draw_landing_gear(ssWidth)
   draw_flaps(ssWidth)
   draw_speedbrakes(ssWidth)
   draw_brakes(ssWidth)
-
---  graphics.set_color(1,1,1,1)
---  draw_string_Helvetica_18(12, 145, string.format("%3.2f", COM1/100))
-
 
 end
 
